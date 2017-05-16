0a72a3bcf8f25ede76cda8005636b655c92f3d4e
Jhon Vega <jhonvegamas@gmail.com>
2017-05-16 07:44:38 -0500

    Modificación de pruebas unitarias con respecto a la nueva funcionalidad

--- application/tests/TournamentTest.php
+++ application/tests/TournamentTest.php
@@ -15,9 +15,33 @@ class TournamentTest extends PHPUnit_Framework_TestCase
         $tournament = new Tournament('UPTC', 'Torneo de la U', '/img/foto.png', '#ffff');
         $this->assertInstanceOf(Tournament::class, $tournament);
     }
-    
-    function test_is_should_equal_tournament(){
-        
+
+    function test_it_should_have_a_tournament_name()
+    {
+        $tournament = new Tournament('UPTC', 'Torneo de la U', '/img/foto.png', '#ffff');
+        $name = $tournament->getName();
+        $this->assertEquals('UPTC', $name);
     }
 
+    function test_it_should_have_a_tournament_description()
+    {
+        $tournament = new Tournament('UPTC', 'Torneo de la U', '/img/foto.png', '#ffff');
+        $name = $tournament->getDescription();
+        $this->assertEquals('Torneo de la U', $name);
+    }
+    
+    function test_it_should_have_a_tournament_image_url()
+    {
+        $tournament = new Tournament('UPTC', 'Torneo de la U', '/img/foto.png', '#ffff');
+        $name = $tournament->getImage_url();
+        $this->assertEquals('/img/foto.png', $name);
+    }
+    
+    function test_it_should_have_a_tournament_color()
+    {
+        $tournament = new Tournament('UPTC', 'Torneo de la U', '/img/foto.png', '#ffff');
+        $name = $tournament->getColor();
+        $this->assertEquals('#ffff', $name);
+    }
+    
 }
